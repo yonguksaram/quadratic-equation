9c945dd6bfb0bbf4cc44c0d434c02666abbc5bbf
Stanislau Alimpau <alimberg@list.ru>
2018-02-26 18:52:11 +0300

    first

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  var parts = equation.split(" ");
+  if (parts[7]=="-") { parts[8]=0-parts[8];};
+  if (parts[3]=="-") { parts[4]=0-parts[4];};
+  var a = +parts[0];
+  var b = +parts[4];
+  var c = +parts[8];
+  var y = (-b + Math.sqrt(Math.pow(b,2)-4*a*c))/(2*a);
+  var x = (-b - Math.sqrt(Math.pow(b,2)-4*a*c))/(2*a);
+  return [x,y];
 }
