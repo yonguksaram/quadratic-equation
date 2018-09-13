b65fb7bc0d5c982260dc2b1a243ab382fa1d39b3
Stanislau Alimpau <alimberg@list.ru>
2018-09-13 23:04:04 +0300

    quadratic-equation/adding code

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
 module.exports = function solveEquation(equation) {
   let parts = equation.split(" ");
-  if (parts[7]=="-") { parts[8]=0-parts[8];};
-  if (parts[3]=="-") { parts[4]=0-parts[4];};
   let a = +parts[0];
-  let b = +parts[4];
-  let c = +parts[8];
+  let b = +(parts[3] + parts[4]);
+  let c = +(parts[7] + parts[8]);
   let y = Math.round((-b - Math.sqrt(Math.pow(b,2)-4*a*c))/(2*a));
   let x = Math.round((-b + Math.sqrt(Math.pow(b,2)-4*a*c))/(2*a));
-  let finalArr = [x,y];
-  return finalArr.sort (function(a, b) {
-    return a - b;
-  })
+  return [x,y].sort ((a, b) => a - b);
 }
